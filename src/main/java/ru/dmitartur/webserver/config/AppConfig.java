58376e7f7e732715c58bc1e5f2ea39f6948b50f1
Victor Troyan <v_troyan@nposinergy.ru>
2018-08-13 14:47:37 +0300

    - Перекоючил на postgres - Почистил pom - дополнил appConfig

--- src/main/java/ru/dmitartur/webserver/config/AppConfig.java
+++ src/main/java/ru/dmitartur/webserver/config/AppConfig.java
@@ -4,6 +4,9 @@ package ru.dmitartur.webserver.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,7 +18,12 @@ import ru.dmitartur.socketserver.SocketServerInit;
 @EntityScan( basePackages = {"ru.dmitartur.webserver"} )
 @EnableWebSecurity
 @EnableTransactionManagement
-public class AppConfig {
+public class AppConfig extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AppConfig.class);
+    }
     public static void main(String[] args) {
         SocketServerInit socketServerInit = new SocketServerInit();
         SpringApplication.run(AppConfig.class, args);
